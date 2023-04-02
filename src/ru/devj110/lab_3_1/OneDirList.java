f3aeb72d143e0509abb0f4e8ef9d5adcbd22a53d
MSnih <SnihMV@gmail.com>
2023-04-02 18:04:19 +0300

    2 commit

--- src/ru/devj110/lab_3_1/OneDirList.java
+++ src/ru/devj110/lab_3_1/OneDirList.java
@@ -13,14 +13,72 @@ public class OneDirList {
             head = tail = new ListItem(value);
     }
 
-    public void addToHead(Object value){
-        if (head==null)
-            head=tail=new ListItem(value);
+    public void addToHead(Object value) {
+        if (head == null)
+            head = tail = new ListItem(value);
         else {
             ListItem newHead = new ListItem(value);
             newHead.next = head;
             head = newHead;
         }
+    }
+
+    public Object peekFromHead() {
+        return head == null ? null : head.value;
+    }
+
+    public Object peekFromTail() {
+        return tail == null ? null : tail.value;
+    }
+
+    public Object popFromHead() {
+        if (head == null)
+            return null;
+        Object res = head.value;
+        if (head == tail)
+            head = tail = null;
+        else head = head.next;
+        return res;
+    }
+
+    public Object popFromTail() {
+        if (tail == null)
+            return null;
+        if (head == tail) {
+            Object res = tail.value;
+            head = tail = null;
+            return res;
+        }
+        ListItem target = head;
+        while (target.next.next != null) {
+            target = target.next;
+        }
+        Object res = target.next.value;
+        target.next = null;
+        tail = target;
+        return res;
+    }
+
+    public boolean contains(Object value) {
+        ListItem target = head;
+        while (target != null) {
+            if (target.value.equals(value))
+                return true;
+            target = target.next;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void print() {
+        ListItem target = head;
+        while (target != null) {
+            System.out.print(target.value.toString() + (target.next == null ? "" : " -> "));
+        }
+
     }
 
 
