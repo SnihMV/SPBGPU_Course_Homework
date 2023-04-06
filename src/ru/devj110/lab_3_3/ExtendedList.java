d36471b91738cc4ff9719afda54d74a7be3f1753
MSnih <SnihMV@gmail.com>
2023-04-06 13:05:58 +0300

    commit

--- src/ru/devj110/lab_3_3/ExtendedList.java
+++ src/ru/devj110/lab_3_3/ExtendedList.java
@@ -1,12 +1,26 @@
 package ru.devj110.lab_3_3;
 
-import java.util.Arrays;
-
 public class ExtendedList {
 
     private final int capacity;
     private Node head;
     private Node tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getHeadNext() {
+        return head.next;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public Node getTailNext() {
+        return tail.next;
+    }
 
     public ExtendedList() {
         this(10);
@@ -17,7 +31,12 @@ public class ExtendedList {
     }
 
     public void addToHead(Object value) {
-
+        if (head != null) {
+            if ((tail.next = head.addToHead(value)) != null)
+                tail = tail.next;
+            return;
+        }
+        head = tail = new Node(value);
     }
 
     public void addToTail(Object value) {
@@ -33,43 +52,61 @@ public class ExtendedList {
     }
 
 
-    public void print(){
+    public void print() {
         Node aim = head;
-        while (aim!=null){
-            System.out.print(aim.toString() + (aim.next!=null?" --> ":""));
-            aim=aim.next;
+        while (aim != null) {
+            System.out.print(aim.toString() + (aim.next != null ? " --> " : ""));
+            aim = aim.next;
         }
         System.out.println();
     }
 
     private class Node {
         Object[] array;
-        int volume;
+        int size;
         Node next;
+
 
         Node(Object value) {
             this.array = new Object[capacity];
             array[0] = value;
-            this.volume = 1;
+            this.size = 1;
         }
 
         void addToTail(Object value) {
-            array[volume++] = value;
+            array[size++] = value;
         }
 
-        void addToHead(Object value) {
-            System.arraycopy(array, 0, array, 1, volume);
+        Node addToHead(Object value) {
+            if (!isFull()) {
+                System.arraycopy(array, 0, array, 1, size++);
+                array[0] = value;
+                return null;
+            }
+            Object excess = array[size - 1];
+            System.arraycopy(array, 0, array, 1, size - 1);
             array[0] = value;
-            volume++;
+            if (next==null)
+                return new Node(excess);
+            return next.addToHead(excess);
+        }
+
+        Object popFromTail() {
+            return array[--size];
         }
 
         @Override
         public String toString() {
-            return Arrays.toString(array);
+            StringBuilder sb = new StringBuilder("[");
+            for (int i = 0; i < size; i++) {
+                sb.append((array[i] != null ? array[i].toString() : "null") + (i < size - 1 ? "," : ""));
+            }
+            sb.append("]");
+            return sb.toString();
         }
 
         public boolean isFull() {
-            return volume == capacity;
+            return size == capacity;
         }
     }
 }
