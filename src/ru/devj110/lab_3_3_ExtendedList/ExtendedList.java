c267a6e31d5e2793053985f6548640724548e7fa
MSnih <SnihMV@gmail.com>
2023-05-03 00:24:39 +0300

    commit

--- src/ru/devj110/lab_3_3_ExtendedList/ExtendedList.java
+++ src/ru/devj110/lab_3_3_ExtendedList/ExtendedList.java
@@ -1,38 +1,41 @@
 package ru.devj110.lab_3_3_ExtendedList;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class ExtendedList {
+import static java.lang.Math.min;
+
+public class ExtendedList<E> implements Iterable<E> {
 
     private static final int DEFAULT_NODE_CAPACITY = 10;
     private final int nodeCapacity;
-    private Node head;
-    private Node tail;
+    private Node<E> head;
+    private Node<E> tail;
 
 
     public ExtendedList(int nodeCapacity) {
         if (nodeCapacity <= 0)
             throw new IllegalArgumentException("Node capacity must be > 0");
         this.nodeCapacity = nodeCapacity;
-        head = tail = new Node();
+        head = tail = new Node<E>();
     }
 
     public ExtendedList() {
         this(DEFAULT_NODE_CAPACITY);
     }
 
-    public ExtendedList(Object[] array, int nodeCapacity) {
+    public ExtendedList(E[] array, int nodeCapacity) {
         this(nodeCapacity);
         int rest = array.length;
-        Node node = head;
+        Node<E> node = head;
         while (true) {
             int srcPos = array.length - rest;
-            int copiedLength = Math.min(nodeCapacity, rest);
+            int copiedLength = min(nodeCapacity, rest);
             System.arraycopy(array, srcPos, node.dataHolder, 0, copiedLength);
             rest -= copiedLength;
             node.size += copiedLength;
             if (rest > 0) {
-                node.next = new Node();
+                node.next = new Node<E>();
                 node = node.next;
             } else {
                 tail = node;
@@ -41,13 +44,13 @@ public class ExtendedList {
         }
     }
 
-    public ExtendedList(Object[] array) {
+    public ExtendedList(E[] array) {
         this(array, DEFAULT_NODE_CAPACITY);
     }
 
-    public void addToHead(Object value) {
-        Node node = head;
-        Object tmp;
+    public void addToHead(E value) {
+        Node<E> node = head;
+        E tmp;
         while (node != null) {
             if (!node.isFull()) {
                 node.addToHead(value);
@@ -57,7 +60,7 @@ public class ExtendedList {
             node.addToHead(value);
             value = tmp;
             if (node == tail) {
-                tail = new Node(value);
+                tail = new Node<E>(value);
                 node.next = tail;
                 return;
             }
@@ -65,46 +68,46 @@ public class ExtendedList {
         }
     }
 
-    public void recursiveAddToHead(Object value) {
+    public void recursiveAddToHead(E value) {
         if ((tail.next = head.recursiveAddToHead(value)) != null)
             tail = tail.next;
     }
 
-    public void addToTail(Object value) {
+    public void addToTail(E value) {
         if (!tail.isFull())
             tail.addToTail(value);
         else
-            tail = tail.next = new Node(value);
+            tail = tail.next = new Node<E>(value);
     }
 
-    public Object peekFromHead() {
+    public E peekFromHead() {
         if (!head.isEmpty())
             return head.peekFirst();
         throw new NoSuchElementException("List is empty");
     }
 
-    public Object peekFromTail() {
+    public E peekFromTail() {
         if (!tail.isEmpty())
             return tail.peekLast();
         throw new NoSuchElementException("List is empty");
     }
 
-    public Object popFromHead() {
+    public E popFromHead() {
         if (head.isEmpty())
             throw new NoSuchElementException("List is empty. Nothing to pop from its head");
-        Node node = head;
-        Object res = node.popElement(0);
+        Node<E> node = head;
+        E res = node.popElement(0);
         pullUp(node);
         return res;
     }
 
-    public Object popFromTail() {
+    public E popFromTail() {
         if (tail.isEmpty())
             throw new NoSuchElementException("List is empty. Nothing to pop from its tail");
-        Object res = tail.popLast();
+        E res = tail.popLast();
         if (tail.isEmpty()) {
             if (head != tail) {
-                Node node = head;
+                Node<E> node = head;
                 while (!node.next.isEmpty())
                     node = node.next;
                 node.next = null;
@@ -114,36 +117,43 @@ public class ExtendedList {
         return res;
     }
 
-    public boolean contains(Object value) {
+    private Node<E> findNodeWith(E value) {
         if (!head.isEmpty()) {
-            Node node = head;
+            Node<E> node = head;
             while (node != null) {
-                if (node.contains(value) != -1)
-                    return true;
+                if (node.contains(value))
+                    return node;
                 node = node.next;
             }
         }
-        return false;
+        return null;
+    }
+
+    public boolean contains(E value) {
+        return findNodeWith(value) != null;
     }
 
     public boolean isEmpty() {
         return head.isEmpty();
     }
 
-    public void remove(Object value) {
+    public void remove(E value) {
         if (isEmpty())
             return;
-        Node node = head;
+        Node<E> node = head;
+
         while (node != null) {
-            if (node.remove(value)) {
+            int pos = node.getPosWith(value);
+            if (pos >= 0) {
+                node.popElement(pos);
                 pullUp(node);
-                break;
+                return;
             }
             node = node.next;
         }
     }
 
-    private void pullUp(Node node) {
+    private void pullUp(Node<E> node) {
         while (node.next != null) {
             node.pullUp();
             if (node.next.isEmpty()) {
@@ -155,84 +165,88 @@ public class ExtendedList {
         }
     }
 
-    public void addArrayToHeadSimple(Object[] array) {
+    public void addArrayToHeadSimple(E[] array) {
         for (int i = array.length - 1; i >= 0; i--) {
             addToHead(array[i]);
         }
     }
 
-    public void addArrayToTailSimple(Object[] array) {
+    public void addArrayToTailSimple(E[] array) {
         for (int i = 0; i < array.length; i++) {
             addToTail(array[i]);
         }
     }
 
-    public void addArrayToHead(Object[] array) {
+    public void addArrayToHead(E[] array) {
         if (array.length == 0)
             return;
-        ExtendedList that = new ExtendedList(array, nodeCapacity);
+        ExtendedList<E> that = new ExtendedList<>(array, nodeCapacity);
         that.tail.next = this.head;
         this.head = that.head;
-        pullUp(that.tail);
+        if (!that.tail.isFull())
+            pullUp(that.tail);
     }
 
-    public void addArrayToTail(Object[] array) {
+    public void addArrayToTail(E[] array) {
         if (array.length == 0)
             return;
-        ExtendedList that = new ExtendedList(array, nodeCapacity);
+        ExtendedList<E> that = new ExtendedList<>(array, nodeCapacity);
         this.tail.next = that.head;
-        pullUp(this.tail);
+        if (!this.tail.isFull())
+            pullUp(this.tail);
         this.tail = that.tail;
     }
 
-    public void addCollectionToHead(Iterable collection) {
+    public void addCollectionToHead(Iterable<E> collection) {
         if (collection == null)
             throw new IllegalArgumentException("Input collection is null");
-        ExtendedList tmp = new ExtendedList(nodeCapacity);
-        for (Object value : collection) {
+        ExtendedList<E> tmp = new ExtendedList(nodeCapacity);
+        for (E value : collection) {
             tmp.addToTail(value);
         }
         consumeToHead(tmp);
     }
 
-    public void addCollectionToTail(Iterable collection) {
+    public void addCollectionToTail(Iterable<E> collection) {
         if (collection == null)
             throw new IllegalArgumentException("Input collection is null");
-        for (Object value : collection) {
+        for (E value : collection) {
             addToTail(value);
         }
     }
 
-    public void consumeToHead(ExtendedList list) {
-        ExtendedList that = list;
+    public void consumeToHead(ExtendedList<E> list) {
+        ExtendedList<E> that = list;
         if (list.nodeCapacity != this.nodeCapacity)
-            that = getRestructured(list, this.nodeCapacity);
+            that = list.restructure(this.nodeCapacity);
         that.tail.next = this.head;
-        pullUp(that.tail);
+        if (!that.tail.isFull())
+            pullUp(that.tail);
         this.head = that.head;
         list.head = list.tail = null;
     }
 
-    public void consumeToTail(ExtendedList list) {
-        ExtendedList that = list;
-        if (list.nodeCapacity != this.nodeCapacity)
-            that = getRestructured(list, this.nodeCapacity);
+    public void consumeToTail(ExtendedList<E> list) {
+        ExtendedList<E> that = list;
+        if (that.nodeCapacity != this.nodeCapacity)
+            that = list.restructure(this.nodeCapacity);
         this.tail.next = that.head;
-        pullUp(this.tail);
+        if (!this.tail.isFull())
+            pullUp(this.tail);
         this.tail = that.tail;
         list.head = list.tail = null;
     }
 
-    public static ExtendedList getRestructured(ExtendedList list, int nodeCapacity) {
-        ExtendedList tmp = new ExtendedList(nodeCapacity);
-        while (!list.isEmpty()) {
-            tmp.addToTail(list.popFromHead());
+    public ExtendedList<E> restructure(int nodeCapacity) {
+        ExtendedList<E> tmp = new ExtendedList<>(nodeCapacity);
+        while (!isEmpty()) {
+            tmp.addToTail(popFromHead());
         }
         return tmp;
     }
 
-    public void convertAll(Converter c) {
-        Node node = head;
+    public void convertAll(Converter<E, ? extends E> c) {
+        Node<E> node = head;
         while (node != null) {
             node.convert(c);
             node = node.next;
@@ -240,7 +254,7 @@ public class ExtendedList {
     }
 
     public void print() {
-        Node node = head;
+        Node<E> node = head;
         while (node != null) {
             System.out.print(node + (node.next != null ? " --> " : ""));
             node = node.next;
@@ -248,96 +262,96 @@ public class ExtendedList {
         System.out.println();
     }
 
+    @Override
+    public Iterator<E> iterator() {
+        return new XListIterator<E>(head, 0);
+    }
 
-    private class Node {
-        Object[] dataHolder;
+
+    private class Node<E> {
+        E[] dataHolder;
         int size;
-        Node next;
+        Node<E> next;
 
         Node() {
-            this.dataHolder = new Object[nodeCapacity];
+            this.dataHolder = (E[]) new Object[nodeCapacity];
         }
 
-        Node(Object value) {
+        Node(E value) {
             this();
             addToTail(value);
         }
 
-        Node(Object[] array) {
+        Node(E[] array) {
             this();
             if (array.length > nodeCapacity)
                 throw new IllegalArgumentException("Incorrect array length");
-            System.arraycopy(array,0, dataHolder,0, array.length);
+            System.arraycopy(array, 0, dataHolder, 0, array.length);
             this.size += array.length;
         }
 
 
-
-        void addToTail(Object value) {
+        void addToTail(E value) {
             dataHolder[size++] = value;
         }
 
-        void addToHead(Object value) {
+        void addToHead(E value) {
             System.arraycopy(dataHolder, 0, dataHolder, 1, size++);
             dataHolder[0] = value;
         }
 
-        Node recursiveAddToHead(Object value) {
+        Node<E> recursiveAddToHead(E value) {
             if (!isFull()) {
                 addToHead(value);
                 return null;
             }
-            Object excess = dataHolder[size - 1];
+            E excess = dataHolder[size - 1];
             System.arraycopy(dataHolder, 0, dataHolder, 1, size - 1);
             dataHolder[0] = value;
             if (next == null)
-                return new Node(excess);
+                return new Node<E>(excess);
             return next.recursiveAddToHead(excess);
         }
 
-        Object peekFirst() {
+        E peekFirst() {
             return dataHolder[0];
         }
 
-        Object peekLast() {
+        E peekLast() {
             return dataHolder[size - 1];
         }
 
-        Object popLast() {
+        E popLast() {
             return dataHolder[--size];
         }
 
-        Object popElement(int pos) {
-            Object res = dataHolder[pos];
+        E popElement(int pos) {
+            E res = dataHolder[pos];
             System.arraycopy(dataHolder, pos + 1, dataHolder, pos, --size - pos);
             return res;
         }
 
-        public boolean remove(Object value) {
-            int i;
-            if ((i = contains(value)) != -1) {
-                popElement(i);
-                return true;
-            }
-            return false;
-        }
 
         public void pullUp() {
             int lack = nodeCapacity - size;
-            int moved = next.size >= lack ? lack : next.size;
+            int moved = min(lack, next.size);
             System.arraycopy(next.dataHolder, 0, this.dataHolder, size, moved);
             System.arraycopy(next.dataHolder, moved, next.dataHolder, 0, next.size - moved);
             this.size += moved;
             next.size -= moved;
         }
 
-        public void convert(Converter c) {
+        public void convert(Converter<E, ? extends E> c) {
             for (int i = 0; i < size; i++) {
                 dataHolder[i] = c.convert(dataHolder[i]);
             }
         }
 
-        public int contains(Object value) {
+        public boolean contains(E value) {
+            return getPosWith(value) != -1;
+        }
+
+        private int getPosWith(E value) {
             for (int i = 0; i < size; i++) {
                 if (dataHolder[i] != null && dataHolder[i].equals(value) || dataHolder[i] == null && value == null)
                     return i;
@@ -361,6 +375,33 @@ public class ExtendedList {
             }
             sb.append("]");
             return sb.toString();
+        }
+    }
+
+    private class XListIterator<E> implements Iterator<E> {
+        private Node<E> nxtNode;
+        private int nxtPos;
+
+        public XListIterator(Node<E> startNode, int startPos) {
+            this.nxtNode = startNode;
+            this.nxtPos = startPos;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return nxtNode.size - 1 >= nxtPos;
+        }
+
+        @Override
+        public E next() {
+            E res = nxtNode.dataHolder[nxtPos];
+            if (nxtNode.size - 1 == nxtPos && nxtNode.next != null) {
+                nxtNode = nxtNode.next;
+                nxtPos = 0;
+            } else
+                nxtPos++;
+            return res;
         }
     }
 }
