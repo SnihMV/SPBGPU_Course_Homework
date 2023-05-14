5a7af34500d20f9eb86ef73056f8684950328e37
MSnih <SnihMV@gmail.com>
2023-05-14 23:44:55 +0300

    commit

--- src/ru/devj110/lab_2_1/Main.java
+++ src/ru/devj110/lab_2_1/Main.java
@@ -1,16 +1,24 @@
 package ru.devj110.lab_2_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        Person[] persons = new Person[]{
-                new Teacher("Ronald", "Turner", Gender.MALE,Department.F1,Degree.P,"Programming paradigms"),
-          new Teacher("Ruth", "Hollings", Gender.FEMALE,Department.F2,Degree.C,"Domestic arbitration"),
-                new RegularStudent("Leo","Wilkinson", Gender.MALE,Department.F1,3,Stage.B),
-                new RegularStudent("Anna","Cunningham", Gender.FEMALE,Department.F3,2,Stage.B),
-                new RegularStudent("Jill", "Lundqvist",Gender.FEMALE, Department.F2,1,Stage.M),
-                new PhdStudent("Ronald", "Correa",Gender.MALE,Department.F1,"Design of a functional programming language")
-        };
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Teacher("Ronald", "Turner", Gender.MALE, Department.F1, Degree.P, "Programming paradigms"));
+        persons.add(new Teacher("Ruth", "Hollings", Gender.FEMALE, Department.F2, Degree.C, "Domestic arbitration"));
+        persons.add(new RegularStudent("Leo", "Wilkinson", Gender.MALE, Department.F1, 3, Stage.B));
+//        persons.add(new RegularStudent("Anna", "Cunningham", Gender.FEMALE, Department.F3, 2, Stage.B));
+//        persons.add(new RegularStudent("Jill", "Lundqvist", Gender.FEMALE, Department.F2, 1, Stage.M));
+        persons.add(new PhdStudent("Ronald", "Correa", Gender.MALE, Department.F1, "Design of a functional programming language"));
+
         Person.printAll(persons);
+
+        RegularStudent.loadStudents(persons);
+
+        Person.printAll(persons);
+
     }
 }
--- src/ru/devj110/lab_2_1/RegularStudent.java
+++ src/ru/devj110/lab_2_1/RegularStudent.java
@@ -1,7 +1,9 @@
 package ru.devj110.lab_2_1;
 
 
-public class RegularStudent extends AbstractStudent{
+import java.util.Collection;
+
+public class RegularStudent extends AbstractStudent {
     private int course;
 
     private Stage stage;
@@ -13,17 +15,24 @@ public class RegularStudent extends AbstractStudent{
     }
 
     public void setCourse(int course) {
-        if (course<1 ||course >6) throw new IllegalArgumentException("Course must be from 1 to 6");
+        if (course < 1 || course > 6) throw new IllegalArgumentException("Course must be from 1 to 6");
         this.course = course;
     }
 
     public void setStage(Stage stage) {
-        if (stage==null) throw new IllegalArgumentException("Stage can't be null");
+        if (stage == null) throw new IllegalArgumentException("Stage can't be null");
         this.stage = stage;
     }
 
     @Override
     public String getDetailedInfo() {
-        return getGender().getPronoun()+" is "+course+((course==2)?"nd":"th")+" year "+stage.getTitle()+" student.";
+        return getGender().getPronoun() + " is " + course + ((course == 2) ? "nd" : "th") + " year " + stage.getTitle() + " student.";
+    }
+
+    public static void loadStudents(Collection<? super RegularStudent> collection) {
+        collection.add(new RegularStudent("Anna", "Cunningham", Gender.FEMALE, Department.F3,
+                1, Stage.B));
+        collection.add(new RegularStudent("Jill", "Lundqvist", Gender.FEMALE, Department.F2,
+                1, Stage.M));
     }
 }
