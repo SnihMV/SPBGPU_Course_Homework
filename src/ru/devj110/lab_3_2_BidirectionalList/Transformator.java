423f048d3532e5ece46e6136f4a0a5ce42ca103d
Sneech <Snih@yandex.ru>
2023-05-17 02:06:54 +0300

    lab 2.234 done

--- src/ru/devj110/lab_3_2_BidirectionalList/Transformator.java
+++ src/ru/devj110/lab_3_2_BidirectionalList/Transformator.java
@@ -1,5 +1,5 @@
 package ru.devj110.lab_3_2_BidirectionalList;
 
-public interface Transformator {
-    Object transform(Object value);
+public interface Transformator <T,R> {
+    R transform(T value);
 }
--- src/ru/devj110/lab_3_2_BidirectionalList/TwoDirList.java
+++ src/ru/devj110/lab_3_2_BidirectionalList/TwoDirList.java
@@ -1,12 +1,15 @@
 package ru.devj110.lab_3_2_BidirectionalList;
 
-public class TwoDirList {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-    private ListItem head;
-    private ListItem tail;
+public class TwoDirList<T> implements Iterable<T> {
 
-    public void addToHead(Object value) {
-        ListItem newHead = new ListItem(value);
+    private ListItem<T> head;
+    private ListItem<T> tail;
+
+    public void addToHead(T value) {
+        ListItem<T> newHead = new ListItem<>(value);
         if (head != null) {
             head.prev = newHead;
             newHead.next = head;
@@ -15,8 +18,8 @@ public class TwoDirList {
             head = tail = newHead;
     }
 
-    public void addToTail(Object value) {
-        ListItem newTail = new ListItem(value);
+    public void addToTail(T value) {
+        ListItem<T> newTail = new ListItem<>(value);
         if (tail != null) {
             tail.next = newTail;
             newTail.prev = tail;
@@ -25,17 +28,21 @@ public class TwoDirList {
             head = tail = newTail;
     }
 
-    public Object peekFromHead() {
-        return head != null ? head.value : null;
+    public T peekFromHead() {
+        if (!isEmpty())
+            return head.value;
+        throw new NoSuchElementException("List is empty. Nothing to peek from its head");
     }
 
-    public Object peekFromTail() {
-        return tail != null ? tail.value : null;
+    public T peekFromTail() {
+        if (!isEmpty())
+            return tail.value;
+        throw new NoSuchElementException("List is empty. Nothing to peek from its tail");
     }
 
-    public Object popFromHead() {
-        if (head != null) {
-            Object res = head.value;
+    public T popFromHead() {
+        if (!isEmpty()) {
+            T res = head.value;
             if (head != tail) {
                 head = head.next;
                 head.prev = null;
@@ -43,12 +50,12 @@ public class TwoDirList {
                 head = tail = null;
             return res;
         }
-        throw new IllegalArgumentException("List is empty. Nothing to pop from its head");
+        throw new NoSuchElementException("List is empty. Nothing to pop from its head");
     }
 
-    public Object popFromTail() {
-        if (tail != null) {
-            Object res = tail.value;
+    public T popFromTail() {
+        if (!isEmpty()) {
+            T res = tail.value;
             if (tail != head) {
                 tail = tail.prev;
                 tail.next = null;
@@ -56,31 +63,29 @@ public class TwoDirList {
                 head = tail = null;
             return res;
         }
-        throw new IllegalArgumentException("List is empty. Nothing to pop from its tail");
+        throw new NoSuchElementException("List is empty. Nothing to pop from its tail");
     }
 
     public boolean isEmpty() {
         return head == null;
     }
 
-    public boolean contains(Object value) {
-        ListItem aim = head;
-        while (aim != null) {
-            if (aim.isTheSame(value))
-                return true;
-            aim = aim.next;
-        }
-        return false;
+    public boolean contains(T value) {
+        return getItemWith(value) != null;
     }
 
-    public void remove(Object value) {
-        if (head == null)
+    public void remove(T value) {
+        if (isEmpty())
             return;
         if (head.isTheSame(value)) {
-            popFromHead();
+            if (head != tail) {
+                head = head.next;
+                head.prev = null;
+            } else
+                head = tail = null;
             return;
         }
-        ListItem aim = head.next;
+        ListItem<T> aim = head.next;
         while (aim != null) {
             if (aim.isTheSame(value)) {
                 if (aim != tail) {
@@ -88,18 +93,17 @@ public class TwoDirList {
                     aim.next.prev = aim.prev;
                     return;
                 } else {
-                    tail.prev.next = null;
                     tail = tail.prev;
+                    tail.next = null;
                     return;
                 }
             }
             aim = aim.next;
         }
-
     }
 
     public void printList() {
-        ListItem aim = head;
+        ListItem<T> aim = head;
         while (aim != null) {
             System.out.print(aim + (aim.next != null ? " --> " : ""));
             aim = aim.next;
@@ -108,7 +112,7 @@ public class TwoDirList {
     }
 
     public void reversePrint() {
-        ListItem aim = tail;
+        ListItem<T> aim = tail;
         while (aim != null) {
             System.out.print(aim + (aim.prev != null ? " --> " : ""));
             aim = aim.prev;
@@ -116,59 +120,55 @@ public class TwoDirList {
         System.out.println();
     }
 
-    public void transform(Transformator t) {
-        ListItem aim = head;
+    public void transform(Transformator<T, ? extends T> t) {
+        ListItem<T> aim = head;
         while (aim != null) {
             aim.value = t.transform(aim.value);
             aim = aim.next;
         }
     }
 
-    public void reverseTransform(Transformator t) {
-        ListItem aim = tail;
+    public void reverseTransform(Transformator<T, ? extends T> t) {
+        ListItem<T> aim = tail;
         while (aim != null) {
             aim.value = t.transform(aim.value);
             aim = aim.prev;
         }
     }
 
-    public void addArrayToHead(Object[] array) {
-        if (array == null)
-            return;
-        for (int i = array.length - 1; i >= 0; i--) {
-            addToHead(array[i]);
+    public void addArrayToHead(T[] array) {
+        if (array != null)
+            for (int i = array.length - 1; i >= 0; i--)
+                addToHead(array[i]);
+    }
+
+    public void addArrayToTail(T[] array) {
+        if (array != null)
+            for (int i = 0; i < array.length; i++)
+                addToTail(array[i]);
+    }
+
+    public void addCollectionToHead(Iterable<? extends T> collection) {
+        if (collection != null) {
+            TwoDirList<T> tmpList = new TwoDirList<>();
+            for (T value : collection) {
+                tmpList.addToTail(value);
+            }
+            if (tmpList.head != null) {
+                tmpList.tail.next = this.head;
+                this.head = tmpList.head;
+            }
         }
     }
 
-    public void addArrayToTail(Object[] array) {
-        if (array == null) return;
-        for (int i = 0; i < array.length; i++) {
-            addToTail(array[i]);
-        }
+    public void addCollectionToTail(Iterable<? extends T> collection) {
+        if (collection != null)
+            for (T value : collection)
+                addToTail(value);
+
     }
 
-    public void addCollectionToHead(Iterable collection) {
-        if (collection == null)
-            return;
-        TwoDirList tmpList = new TwoDirList();
-        for (Object o : collection) {
-            tmpList.addToTail(o);
-        }
-        if (tmpList.head != null) {
-            tmpList.tail.next = this.head;
-            this.head = tmpList.head;
-        }
-    }
-
-    public void addCollectionToTail(Iterable collection) {
-        if (collection == null)
-            return;
-        for (Object value : collection) {
-            addToTail(value);
-        }
-    }
-
-    public void consumeToHead(TwoDirList that) {
+    public void consumeToHead(TwoDirList<T> that) {
         if (that == null || that.head == null)
             return;
         that.tail.next = this.head;
@@ -177,7 +177,7 @@ public class TwoDirList {
         that.head = that.tail = null;
     }
 
-    public void consumeToTail(TwoDirList that) {
+    public void consumeToTail(TwoDirList<T> that) {
         if (that == null || that.head == null)
             return;
         that.head.prev = this.tail;
@@ -186,16 +186,51 @@ public class TwoDirList {
         that.head = that.tail = null;
     }
 
-    private static class ListItem {
-        Object value;
-        ListItem prev;
-        ListItem next;
+    @Override
+    public Iterator iterator() {
+        return new ForwardIterator(head);
+    }
 
-        public ListItem(Object value) {
+    public Iterable<T> after(T value) {
+        return () -> new ForwardIterator(getItemWith(value));
+    }
+
+    public Iterable<T> until(T value) {
+        return () -> new ForwardIterator<>(head, getItemWith(value));
+    }
+
+    public Iterable<T> backward() {
+        return () -> new BackwardIterator<>(tail);
+    }
+
+    public Iterable<T> backFrom(T value) {
+        return () -> new BackwardIterator<>(getItemWith(value));
+    }
+
+    public Iterable<T> backUntil(T value){
+        return () -> new BackwardIterator<>(tail, getItemWith(value));
+    }
+
+    private ListItem<T> getItemWith(T value) {
+        ListItem<T> aim = head;
+        while (aim != null) {
+            if (aim.isTheSame(value))
+                return aim;
+            aim = aim.next;
+        }
+        return null;
+    }
+
+    private static class ListItem<T> {
+        T value;
+        ListItem<T> prev;
+        ListItem<T> next;
+
+        public ListItem(T value) {
             this.value = value;
         }
 
-        public boolean isTheSame(Object value) {
+        public boolean isTheSame(T value) {
             return (this.value != null && this.value.equals(value)) || (this.value == null && value == null);
         }
 
@@ -204,4 +239,50 @@ public class TwoDirList {
             return value != null ? value.toString() : null;
         }
     }
+
+    private class ForwardIterator<T> implements Iterator<T> {
+
+        protected ListItem<T> nextItem;
+        protected ListItem<T> finalItem;
+
+        public ForwardIterator(ListItem<T> startItem, ListItem<T> finalItem) {
+            this.nextItem = startItem;
+            this.finalItem = finalItem;
+        }
+
+        public ForwardIterator(ListItem<T> startItem) {
+            this(startItem, null);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextItem != null && !nextItem.equals(finalItem);
+        }
+
+        @Override
+        public T next() {
+            T res = nextItem.value;
+            nextItem = nextItem.next;
+            return res;
+        }
+    }
+
+    private class BackwardIterator<T> extends ForwardIterator<T> {
+
+        public BackwardIterator(ListItem<T> startItem, ListItem<T> finalItem) {
+            super(startItem, finalItem);
+        }
+
+        public BackwardIterator(ListItem<T> startItem) {
+            super(startItem);
+        }
+
+        @Override
+        public T next() {
+            T res = nextItem.value;
+            nextItem = nextItem.prev;
+            return res;
+        }
+    }
+
 }
