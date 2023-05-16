c5ef17746317f7fda56926a3e0d53c7357d78635
Sneech <Snih@yandex.ru>
2023-05-17 02:06:54 +0300

    lab 2.234 done

--- src/ru/devj110/lab_3_2_BidirectionalList/Main.java
+++ src/ru/devj110/lab_3_2_BidirectionalList/Main.java
@@ -3,21 +3,17 @@ package ru.devj110.lab_3_2_BidirectionalList;
 
 public class Main {
     public static void main(String[] args) {
-        TwoDirList list = new TwoDirList();
-        list.addToHead(1);
+        TwoDirList<String> list = new TwoDirList<>();
+        list.addToHead("1");
         list.addToHead("bfr");
-        list.addToHead('c');
         list.addToHead(null);
+        list.addToHead("gg");
+        list.addToHead("tt");
+        list.addToHead("u");
         list.printList();
-        TwoDirList list1 = new TwoDirList();
-        list1.addToHead("gg");
-        list1.addToHead('F');
-        list1.addToHead(42);
-        list1.printList();
-        list.consumeToHead(list1);
-        list.printList();
-        list.reversePrint();
-
+        for (String s : list.backUntil("u")) {
+            System.out.println(s);
+        }
 
 
     }
